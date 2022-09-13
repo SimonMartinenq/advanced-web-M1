05724c85c620936e1f666dbfb079bc34be029b12
AstrydCasimir <astryd.casimirgressier@gmail.com>
2022-09-13 17:46:24 +0200

    Ex 3: remove vue CLI placeholders

---
+++ vue-oauth-microsoft-graph/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
